872d68b3d229484d4ee6bd14b15fd2fc1580e11c
samsamkim <hoonshik10@gmail.com>
2018-05-24 22:35:00 +0000

    user login/register feature

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   resources :products, only: [:index, :show]
   resources :categories, only: [:show]
 
+
   resource :cart, only: [:show] do
     put    :add_item
     delete :remove_item
@@ -21,6 +22,30 @@ Rails.application.routes.draw do
     resources :categories, except: [:edit, :update, :delete]
   end
 
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  get '/logout' => 'sessions#destroy'
+
+  get '/signup' => 'users#new'
+  post '/users' => 'users#create'
+
+
+  # GifVault::Application.routes.draw do
+
+  #     # This route sends requests to our naked url to the *cool* action in the *gif* controller.
+  #     root to: 'gif#cool'
+
+  #     # I've created a gif controller so I have a page I can secure later.
+  #     # This is optional (as is the root to: above).
+  #     get '/cool' => 'gif#cool'
+  #     get '/sweet' => 'gif#sweet'
+
+  #     # These routes will be for signup. The first renders a form in the browse, the second will
+  #     # receive the form and create a user in our database using the data given to us by the user.
+  #     get '/signup' => 'users#new'
+  #     post '/users' => 'users#create'
+
+  # end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
